b8dfbc186f2b9d7c3c36b40ce58f112c35d0534f
MJCarvajalQ <144226562+MJCarvajalQ@users.noreply.github.com>
2026-08-16 17:41:15 +0200

    feat: set up spring boot with sqlite

---
+++ backend/src/main/java/com/hardwarehub/HardwareHubApplication.java
@@ -0,0 +1,12 @@
+package com.hardwarehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareHubApplication.class, args);
+    }
+}
